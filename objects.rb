a42aa589e2ab58e86ef7041fc039b0d4c3aee82c
Nick <komarnickolas@gmail.com>
2017-06-22 10:55:47 -0600

    Updates

--- objects.rb
+++ objects.rb
@@ -52,3 +52,48 @@ elevator.go_up
 elevator.go_down
 elevator.go_down
 elevator.go_down
+
+def reverse(arr, i, j)
+  while i < j
+    temp = arr[i]
+    arr[i] = arr[j]
+    arr[j] = temp
+    i+=1
+    j-=1
+  end
+end
+
+def Rotate(arr, n)
+  n %= arr.length
+  reverse arr, 0, arr.length-1
+  reverse arr, 0, n-1
+  reverse arr, n, arr.length-1
+  puts arr.to_s
+end
+
+Rotate [1, 2, 3, 4, 5], -5
+Rotate [1, 2, 3, 4, 5], -4
+Rotate [1, 2, 3, 4, 5], -3
+Rotate [1, 2, 3, 4, 5], -2
+Rotate [1, 2, 3, 4, 5], -1
+Rotate [1, 2, 3, 4, 5], 1
+Rotate [1, 2, 3, 4, 5], 2
+Rotate [1, 2, 3, 4, 5], 3
+Rotate [1, 2, 3, 4, 5], 4
+Rotate [1, 2, 3, 4, 5], 5
+
+def Factorial(n)
+  if n > 0
+    temp = 1
+    (2...n+1).each {|i|
+      temp *= i
+    }
+    puts temp
+  else
+    puts 'undefined'
+  end
+end
+
+Factorial -1
+Factorial 6
+Factorial 7
